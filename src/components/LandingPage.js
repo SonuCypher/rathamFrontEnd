178663306759b9465728d5c3f436f381db196148
Cypher890 <abdulsuhaib07@gmail.com>
2023-09-18 16:56:28 +0530

    final fix

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import { Button, Container, Grid, Typography } from "@mui/material";
 import React from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { toggleChatBot } from "../store/chatSlice";
 
 function LandingPage() {
